9dd28021488355c931474a548e6fc13bbeed6281
Alice <aliceviccajee@Alices-MacBook-Air.local>
2018-05-25 14:13:40 +0100

    header positioning

--- functions.php
+++ functions.php
@@ -6,3 +6,19 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'child-theme', get_stylesheet_directory() . '/style.css' );
 
 }
+
+
+//this function removes the search box from the site header
+function remove_storefront_product_search_box(){
+    remove_action( 'storefront_header', 'storefront_product_search', 40 );
+}
+
+add_action('init', 'remove_storefront_product_search_box' );
+
+
+
+function remove_storefront_header_cart(){
+    remove_action( 'storefront_header', 'storefront_header_cart', 60 );
+}
+
+add_action('init', 'remove_storefront_header_cart' );
